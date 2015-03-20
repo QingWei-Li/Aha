3495b5decb7b306432168e38a6a6b92a0af6b602
cinwell.li <cinwell.li@gmail.com>
2015-03-20 14:18:58 +0800

    modify css

--- assets/js/mainController.js
+++ assets/js/mainController.js
@@ -11,13 +11,15 @@ mainApp.controller("mainController", function ($scope) {
         showSelectionCheckbox:true,
 		showFilter: true,
 		columnDefs: [
-			{field: "name", displayName: "名称", width: "**", cellTemplate: "<a class='item-q item-q{{row.getProperty(col.field).q}}' data-id='{{row.getProperty(col.field).id}}'>{{row.getProperty(col.field).title}}</a>"},
+			{field: "name", displayName: "名称", width: "**", cellTemplate: "<a class='cell-content item-q item-q{{row.getProperty(col.field).q}}' data-id='{{row.getProperty(col.field).id}}'>{{row.getProperty(col.field).title}}</a>"},
 			{field: "quality", displayName: "数量", width:'auto'},
 			{field: "similar", displayName: "市场最低价(个)", width: "**", cellTemplate: moneyTemplate},
 			{field: "buyout", displayName: "设置一口价(个)", width: "**", enableCellEdit: true, cellTemplate: moneyTemplate},
-			{field: "type", displayName: "方式"},
-			{field: "quantity", displayName: "堆叠数量"},
-			{field: "stacks", displayName: "堆叠组数"}
+			{field: "name.id", displayName: "方式", cellTemplate: 
+			'<label class="cell-content"><input name="type{{row.getProperty(col.field)}}" type="radio" value=""/>组</label>'+
+			'<label class="cell-content"><input name="type{{row.getProperty(col.field)}}" type="radio" value="" checked="checked"/>个</label>'},
+			{field: "quantity", displayName: "堆叠数量", enableCellEdit: true, cellTemplate: "<div class='text-center'>{{row.getProperty(col.field) || 0}}</div>"},
+			{field: "stacks", displayName: "堆叠组数", enableCellEdit: true, cellTemplate: "<div class='text-center'>{{row.getProperty(col.field) || 0}}</div>"}
 		]
 	};
 	Main = {
